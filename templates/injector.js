0fd3feef9e024e034c222053965111590a6257eb
. <@>
2026-02-06 07:46:39 -0500

    jic

--- templates/injector.js
+++ templates/injector.js
@@ -3,6 +3,7 @@ const iframe = document.querySelector("iframe");
 const doc = iframe.contentDocument;
 doc.body.style.margin = 0;
 doc.body.style.padding = 0;
+doc.body.style.overflow = "hidden";
 const script = doc.createElement("script");
 script.textContent = $script;
 doc.head.appendChild(script);
