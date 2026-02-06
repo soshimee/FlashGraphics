d44f328402384242e153beb03bf2c22e69baf193
. <@>
2026-02-06 07:28:23 -0500

    fix scaling

--- templates/injector.js
+++ templates/injector.js
@@ -1,5 +1,4 @@
 const svg = document.querySelector("svg");
-const fo = document.querySelector("foreignObject");
 const iframe = document.querySelector("iframe");
 const doc = iframe.contentDocument;
 doc.body.style.margin = 0;
@@ -7,11 +6,5 @@ doc.body.style.padding = 0;
 const script = doc.createElement("script");
 script.textContent = $script;
 doc.head.appendChild(script);
-const attrAll = (elems, attr, val) => elems.forEach(e => e.setAttribute(attr, val));
-const ro = new ResizeObserver(entries => {
-	for (const entry of entries) {
-		attrAll([svg, fo, iframe], "width", entry.contentRect.width);
-		attrAll([svg, fo, iframe], "height", entry.contentRect.height);
-	}
-});
-ro.observe(doc.querySelector("ruffle-player"));
+doc.querySelector("ruffle-player").style.width = "100%";
+doc.querySelector("ruffle-player").style.height = "100%";
